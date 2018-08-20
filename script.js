a74bdc22e2f1cf9b2399d153bff26e7f68f485fc
bieltravasssos <gabrielsoarestravassos@hotmail.com>
2018-08-20 10:36:30 -0300

    js teste

---
+++ script.js
@@ -0,0 +1,23 @@
+//let nome = prompt("digite aqui seu nome:");
+
+//console.log(`o nome digitado foi ${nome}`);
+
+//alert(`seja bem vindo ${nome}! aproveite sua visita`);
+
+//let numero = Number(prompt("digite seu numero:"));
+
+//if( isNaN(numero) ){
+  //  console.log("é um numero maior que 10");
+//}
+
+//else{
+  //  console.log("não é um numero maior que 10");
+//}
+
+let limite = Number(prompt("digite o limite do contador"));
+let contador = 1;
+
+while(contador <== limite){
+    console.log(contador);
+    contador = contador + 1;
+}
